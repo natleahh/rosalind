d2eb5d36be1f925e91ff9631efca15a8749509d7
Aminata Tengbe <aminata.tengbe@genomicsengland.co.uk>
2025-08-16 10:12:19 +0100

    test: finalise run_problem tests

--- tests/test_utils/test_run.py
+++ tests/test_utils/test_run.py
@@ -1,12 +1,18 @@
 """Tests for utils related to running rosalin problems."""
 from unittest import mock
-from src.rosalind_problems.utils import run
+from rosalind_problems.utils import run
 
+def function_mock_problem(data_set: str):
+    """FUNCTION - Mock problem function."""
+    return "The Result!"
 
 def test_run_problems_reports_result_to_std_out(capsys):
     """Run problem exports solution to stdout."""
-    mock.Mock(run.get.data_set).return_value((None, None))
+    mock.Mock(run.get.data_set).return_value("")
 
-    run.run_problem(lambda _: "The Result!")
-
+    run.run_problem(
+        func=function_mock_problem,
+        session_id="",
+        token="",
+    )
     assert capsys.readouterr().out == "The Result!"
